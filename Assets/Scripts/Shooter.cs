0aba5bfb2fcbd3d5d35ccbb53985d8ce78d91e51
linkavich14 <5630335+linkavich14@users.noreply.github.com>
2018-08-19 22:42:09 +1000

    Defenders changes

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-    public GameObject projectile, projectileParent, gun;
+    public GameObject projectile, gun;
+    private GameObject projectileParent;
 
-	private void Fire() {
+    private void Start() {
+        projectileParent = GameObject.Find("Projectiles");
+
+        if (!projectileParent) {
+            projectileParent = new GameObject("Projectiles");
+        }
+
+    }
+
+    private void Fire() {
         GameObject newProjectile = Instantiate(projectile) as GameObject;
         newProjectile.transform.parent = projectileParent.transform;
         newProjectile.transform.position = gun.transform.position;
